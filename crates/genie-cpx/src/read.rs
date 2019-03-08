8489616fc8e107b06f37c495586f23f79ea52ca3
Renée Kooi <renee@kooi.me>
2019-03-08 21:40:41 +0100

    cpx: api + doc tweaks

--- crates/genie-cpx/src/read.rs
+++ crates/genie-cpx/src/read.rs
@@ -46,6 +46,7 @@ fn read_scenario_meta<R: Read>(input: &mut R) -> Result<ScenarioMeta> {
     })
 }
 
+/// A campaign file containing scenario files.
 #[derive(Debug, Clone)]
 pub struct Campaign<R>
     where R: Read + Seek
@@ -58,6 +59,10 @@ pub struct Campaign<R>
 impl<R> Campaign<R>
     where R: Read + Seek
 {
+    /// Create a campaign instance from a readable input.
+    ///
+    /// This immediately reads the campaign header and scenario metadata, but not the scenario
+    /// files themselves.
     pub fn from(mut input: R) -> Result<Self> {
         let header = read_campaign_header(&mut input)?;
         let mut entries = vec![];
@@ -72,35 +77,57 @@ impl<R> Campaign<R>
         })
     }
 
+    /// Consume this Campaign instance and get the reader.
     pub fn into_inner(self) -> R {
         self.reader
     }
 
+    /// Get the campaign file version.
     pub fn version(&self) -> f32 {
         self.header.version
     }
 
+    /// Iterate over the scenario metadata for this campaign.
     pub fn entries(&self) -> impl Iterator<Item = &ScenarioMeta> {
         self.entries.iter()
     }
 
+    /// Get the number of scenarios in this campaign.
     pub fn len(&self) -> usize {
         self.entries.len()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.entries.is_empty()
     }
 
     fn get_id(&self, filename: &str) -> Option<usize> {
         self.entries.iter().position(|entry| entry.filename == filename)
     }
 
+    /// Get a scenario by its file name.
+    pub fn by_name(&mut self, filename: &str) -> Result<Scenario> {
+        self.by_name_raw(filename)
+            .map(std::io::Cursor::new)
+            .and_then(|mut buf| Scenario::from(&mut buf))
+    }
+
+    /// Get a scenario by its campaign index.
+    pub fn by_index(&mut self, index: usize) -> Result<Scenario> {
+        self.by_index_raw(index)
+            .map(std::io::Cursor::new)
+            .and_then(|mut buf| Scenario::from(&mut buf))
+    }
+
     /// Get a scenario file buffer by its file name.
-    pub fn by_name(&mut self, filename: &str) -> Result<Vec<u8>> {
+    pub fn by_name_raw(&mut self, filename: &str) -> Result<Vec<u8>> {
         self.get_id(filename)
             .ok_or_else(|| std::io::Error::new(std::io::ErrorKind::NotFound, "scenario not found in campaign"))
-            .and_then(|index| self.by_index(index))
+            .and_then(|index| self.by_index_raw(index))
     }
 
     /// Get a scenario file buffer by its campaign index.
-    pub fn by_index(&mut self, index: usize) -> Result<Vec<u8>> {
+    pub fn by_index_raw(&mut self, index: usize) -> Result<Vec<u8>> {
         let entry = match self.entries.get(index) {
             Some(entry) => entry,
             None => return Err(std::io::Error::new(std::io::ErrorKind::NotFound, "scenario not found in campaign")),
