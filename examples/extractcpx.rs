a971e6ecea860ccee20e6066f39a7128053f5a14
Renée Kooi <renee@kooi.me>
2019-04-08 12:09:16 +0200

    examples: update extractcpx using quicli

--- examples/extractcpx.rs
+++ examples/extractcpx.rs
@@ -1,17 +1,39 @@
 use genie::Campaign;
+use quicli::prelude::*;
+use structopt::StructOpt;
 use std::path::PathBuf;
 use std::fs::File;
 
-fn main() {
-    let filename = std::env::args().nth(1).expect("usage: extractcpx <filename>");
-    let dir = std::env::args().nth(2)
-        .map(PathBuf::from)
-        .unwrap_or_else(|| {
-            std::env::current_dir().expect("invalid cwd")
-        });
-    let dry_run = std::env::args().any(|arg| arg == "-l" || arg == "--list");
+#[derive(Debug, StructOpt)]
+struct List {
+    /// Campaign file.
+    #[structopt(parse(from_os_str))]
+    input: PathBuf,
+}
 
-    let f = File::open(filename).expect("could not open file");
+#[derive(Debug, StructOpt)]
+struct Extract {
+    /// Campaign file.
+    #[structopt(parse(from_os_str))]
+    input: PathBuf,
+    /// Output folder, defaults to cwd.
+    #[structopt(parse(from_os_str))]
+    output: Option<PathBuf>,
+}
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "extractcpx", about = "Campaign file manager")]
+enum Cli {
+    /// List the scenario files in the campaign file.
+    #[structopt(name = "list")]
+    List(List),
+    /// Extract scenario files from the campaign file.
+    #[structopt(name = "extract")]
+    Extract(Extract),
+}
+
+fn list(args: List) {
+    let f = File::open(args.input).expect("could not open file");
     let mut campaign = Campaign::from(f).expect("not a campaign file");
 
     println!("Name: {}", campaign.name());
@@ -25,9 +47,31 @@ fn main() {
     for i in 0..campaign.len() {
         let bytes = campaign.by_index_raw(i).expect("missing scenario data");
         println!("- {} ({} B)", names[i], bytes.len());
+    }
+}
 
-        if !dry_run {
-            std::fs::write(dir.join(&names[i]), bytes).expect("failed to write");
-        }
+fn extract(args: Extract) {
+    let dir = args.output.unwrap_or_else(|| {
+        std::env::current_dir().expect("invalid cwd")
+    });
+
+    let f = File::open(args.input).expect("could not open file");
+    let mut campaign = Campaign::from(f).expect("not a campaign file");
+
+    let names = campaign.entries()
+        .map(|entry| entry.filename.to_string())
+        .collect::<Vec<String>>();
+
+    for i in 0..campaign.len() {
+        let bytes = campaign.by_index_raw(i).expect("missing scenario data");
+        println!("{}", names[i]);
+        std::fs::write(dir.join(&names[i]), bytes).expect("failed to write");
+    }
+}
+
+fn main() {
+    match Cli::from_args() {
+        Cli::List(args) => list(args),
+        Cli::Extract(args) => extract(args),
     }
 }
