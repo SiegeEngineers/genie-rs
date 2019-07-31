48f746b4c5c2aa073e03bd26885fe65922ab3bc3
Renée Kooi <renee@kooi.me>
2019-07-31 18:05:47 +0200

    support: add doc/tests

--- crates/genie-support/src/macros.rs
+++ crates/genie-support/src/macros.rs
@@ -1,4 +1,17 @@
 #[macro_export]
+/// Create an infallible TryInto implementation for an ID container type that returns its contained number as the given target type.
+///
+///
+/// ## Example
+///
+/// ```rust
+/// use genie_support::infallible_try_into;
+/// use std::convert::TryInto;
+/// struct Container(u16);
+/// infallible_try_into!(Container, u32);
+/// let num: u32 = Container(12).try_into().unwrap();
+/// assert_eq!(num, 12u32);
+/// ```
 macro_rules! infallible_try_into {
     ($from:ident, $to:ty) => {
         impl std::convert::TryFrom<$from> for $to {
@@ -11,6 +24,19 @@ macro_rules! infallible_try_into {
 }
 
 #[macro_export]
+/// Create a TryInto implementation for an ID container type that tries to returns its contained number as the given target type.
+///
+/// ## Example
+///
+/// ```rust
+/// use genie_support::fallible_try_into;
+/// use std::convert::{TryFrom, TryInto};
+/// struct Container(u16);
+/// fallible_try_into!(Container, i16);
+/// let num: i16 = Container(12).try_into().unwrap();
+/// assert_eq!(num, 12i16);
+/// assert!(i16::try_from(Container(50000u16)).is_err());
+/// ```
 macro_rules! fallible_try_into {
     ($from:ident, $to:ty) => {
         impl std::convert::TryFrom<$from> for $to {
@@ -23,6 +49,20 @@ macro_rules! fallible_try_into {
 }
 
 #[macro_export]
+/// Create a TryFrom implementation for an ID container type that tries to wrap the given number
+/// type into the container.
+///
+/// ## Example
+///
+/// ```rust
+/// use genie_support::fallible_try_from;
+/// use std::convert::{TryFrom, TryInto};
+/// #[derive(Debug, PartialEq, Eq)]
+/// struct Container(u16);
+/// fallible_try_from!(Container, i16);
+/// assert_eq!(Container::try_from(1i16).unwrap(), Container(1));
+/// assert!(Container::try_from(-1i16).is_err());
+/// ```
 macro_rules! fallible_try_from {
     ($to:ty, $from:ident) => {
         impl std::convert::TryFrom<$from> for $to {
--- crates/genie-support/src/map_into.rs
+++ crates/genie-support/src/map_into.rs
@@ -1,6 +1,20 @@
 /// Helper trait to map a container of T to a container of some type F that implements From<T>.
+///
 /// For example, Result<T> to Result<From<T>>, or Option<T> to Option<From<T>>.
 pub trait MapInto<T> {
+    /// Map the contained `T` into a different type.
+    ///
+    /// Essentially, `.map_into()` is the same as doing `.map(|val| val.into())`.
+    ///
+    /// **Example**
+    ///
+    /// ```rust
+    /// use genie_support::MapInto;
+    ///
+    /// let a: Option<u8> = Some(10);
+    /// let b: Option<u16> = a.map_into();
+    /// assert_eq!(b, Some(10u16));
+    /// ```
     fn map_into(self) -> T;
 }
 
